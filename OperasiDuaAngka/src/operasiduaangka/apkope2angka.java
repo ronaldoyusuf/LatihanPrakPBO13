5a78f6c3aceb8208c6c97398ffefee75d808ec19
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2021-12-09 13:16:40 +0800

    Add files via upload

---
+++ OperasiDuaAngka/src/operasiduaangka/apkope2angka.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package operasiduaangka;
+
+/**
+ *
+ * @author HP
+ */
+public class apkope2angka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
